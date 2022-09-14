3a0991aa576c38960c3e49aa6cc64aa154e128d4
Antônio Carlos <juniorbecari@gmail.com>
2022-09-14 17:50:17 -0300

    Added text

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame
 import random
 
 pygame.init()
+pygame.font.init()
+
+pygame.font.get_init()
+
+pygame.display.set_caption("Racing Game")
+
+font = pygame.font.SysFont(pygame.font.get_default_font(), 40)
 
 screen_dimensions = [840, 650]
 screen = pygame.display.set_mode(screen_dimensions)
@@ -23,6 +30,11 @@ time_count = 0
 time_spawn = 50
 
 enemy_speed = 2
+
+time_sec = 0
+time_min = 0
+
+count = 0
 
 class Entity:
     def __init__(self, x, y, spr):
@@ -55,16 +67,33 @@ class Enemy(Entity):
         
         if self.y > screen_dimensions[1]:
             entities.remove(self)
+        
+        if collide(pygame.Rect(self.x, self.y, self.spr_rect.width, self.spr_rect.height), pygame.Rect(player.x, player.y, self.spr_rect.width, self.spr_rect.height)):
+            game_over()
+
+def game_over():
+    global entities
+    
+    entities.clear()
+    init()
+
+def collide(r1, r2):
+    return r1.left < r2.left + r2.width and r1.left + r1.width > r2.left and r1.top < r2.top + r2.height and r1.top + r1.height > r2.top
 
 def init():
-    global player
+    global player, count, time_sec, time_min
+    
+    time_sec = 0
+    time_min = 0
+    
+    count = 0
     
     player = Car(screen_dimensions[0] / 2, screen_dimensions[1] / 2, player_spr)
     
     entities.append(player)
 
 def tick():
-    global bg_y, time_count, time_spawn
+    global bg_y, time_count, time_spawn, count, time_sec, time_min
     
     for e in entities:
         e.tick()
@@ -84,12 +113,24 @@ def tick():
     elif player.y > screen_dimensions[1] - player.spr_rect.height:
         player.y = screen_dimensions[1] - player.spr_rect.height
     
+    count += 1
+    
+    if count >= 60:
+        count = 0
+        
+        time_sec += 1
+        
+        if time_sec >= 60:
+            time_sec = 0
+            
+            time_min += 1
+    
     time_count += 1
     
     if time_count >= time_spawn:
         time_count = 0
         
-        entities.append(Enemy(random.randint(150, 650), 0, enemy_spr))
+        entities.append(Enemy(random.randint(150, 650), 0 - enemy_spr.get_rect().height, enemy_spr))
 
 def render():
     # Draw bg
@@ -97,6 +138,10 @@ def render():
     
     for e in entities:
         e.render()
+    
+    font_img = font.render("Time: " + ("0" if time_min < 10 else "") + str(time_min) + ":" + ("0" if time_sec < 10 else "") + str(time_sec), True, (255, 255, 255))
+    
+    screen.blit(font_img, (10, 10))
 
 init()
 
